e3900562438696eb079301e8181b9c59dbfb7132
Indragie Karunaratne <i@indragie.com>
2013-02-16 18:38:05 -0700

    more stuff

--- INSOCKSServer/Classes/INSOCKSServer.m
+++ INSOCKSServer/Classes/INSOCKSServer.m
@@ -22,6 +22,8 @@
 //
 
 #import "INSOCKSServer.h"
+#import <netinet/in.h>
+#import <arpa/inet.h>
 
 @interface INSOCKSConnection ()
 - (id)initWithSocket:(GCDAsyncSocket *)socket;
@@ -123,8 +125,21 @@ typedef NS_ENUM(NSInteger, INSOCKS5RequestPhase) {
 	INSOCKS5RequestPhaseAddressType,
 	INSOCKS5RequestPhaseIPv4Address,
 	INSOCKS5RequestPhaseIPv6Address,
+	INSOCKS5RequestPhaseDomainNameLength,
 	INSOCKS5RequestPhaseDomainName,
 	INSOCKS5RequestPhasePort
+};
+
+typedef NS_ENUM(uint8_t, INSOCKS5AddressType) {
+	INSOCKS5AddressTypeIPv4 = 0x01,
+	INSOCKS5AddressTypeIPv6 = 0x04,
+	INSOCKS5AddressTypeDomainName = 0x03
+};
+
+typedef NS_ENUM(uint8_t, INSOCKS5Command) {
+	INSOCKS5CommandConnect = 0x01,
+	INSOCKS5CommandBind = 0x02,
+	INSOCKS5UDPAssociate = 0x03
 };
 
 /*
@@ -192,6 +207,11 @@ static uint8_t const INSOCKS5HandshakeVersion5 = 0x05;
 		unsigned int didEncounterErrorDuringSOCKS5Handshake : 1;
 	} _delegateFlags;
 	uint8_t _numberOfAuthenticationMethods;
+	uint8_t _requestCommandCode;
+	NSMutableData *_addressData;
+	uint8_t _domainNameLength;
+	NSString *_targetHost;
+	NSUInteger _targetPort;
 }
 
 #pragma mark - Initialization
@@ -211,7 +231,7 @@ static uint8_t const INSOCKS5HandshakeVersion5 = 0x05;
 
 - (void)beginSOCKS5Handshake
 {
-	[_socket readDataToLength:1 withTimeout:INSOCKS5SocketTimeout tag:INSOCKS5HandshakePhaseVersion];
+	[self readDataForSOCKS5Tag:INSOCKS5HandshakePhaseVersion];
 }
 
 #pragma mark - Accessors
@@ -236,61 +256,76 @@ static uint8_t const INSOCKS5HandshakeVersion5 = 0x05;
 
 - (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
 {
+	NSUInteger length = [self.class dataLengthForSOCKS5Tag:tag];
+	if (!length) return;
 	switch (tag) {
 		case INSOCKS5HandshakePhaseVersion:
-			[self readSOCKS5VersionFromData:data];
+			[self readSOCKS5VersionFromData:data expectedLength:length];
 			break;
 		case INSOCKS5HandshakePhaseNumberOfAuthenticationMethods:
-			[self readSOCKS5NumberOfAuthenticationMethodsFromData:data];
+			[self readSOCKS5NumberOfAuthenticationMethodsFromData:data expectedLength:length];
 			break;
 		case INSOCKS5HandshakePhaseAuthenticationMethod:
 			[self readSOCKS5AuthenticationMethodsFromData:data];
 			break;
 		case INSOCKS5RequestPhaseHeaderFragment:
-			[self readSOCKS5HeaderFragmentFromData:data];
+			[self readSOCKS5HeaderFragmentFromData:data expectedLength:length];
 			break;
 		case INSOCKS5RequestPhaseAddressType:
-			[self readSOCKS5AddressTypeFromData:data];
+			[self readSOCKS5AddressTypeFromData:data expectedLength:length];
 			break;
 		case INSOCKS5RequestPhaseIPv4Address:
-			[self readSOCKS5IPv4AddressFromData:data];
+			[self readSOCKS5IPv4AddressFromData:data expectedLength:length];
 			break;
 		case INSOCKS5RequestPhaseIPv6Address:
-			[self readSOCKS5IPv6AddressFromData:data];
+			[self readSOCKS5IPv6AddressFromData:data expectedLength:length];
+			break;
+		case INSOCKS5RequestPhaseDomainNameLength:
+			[self readSOCKS5DomainNameLengthFromData:data expectedLength:length];
 			break;
 		case INSOCKS5RequestPhaseDomainName:
 			[self readSOCKS5DomainNameFromData:data];
 			break;
 		case INSOCKS5RequestPhasePort:
-			[self readSOCKS5PortFromData:data];
+			[self readSOCKS5PortFromData:data expectedLength:length];
 			break;
 		default:
 			break;
 	}
 }
 
-- (void)readSOCKS5VersionFromData:(NSData *)data
+- (void)readSOCKS5VersionFromData:(NSData *)data expectedLength:(NSUInteger)length
 {
-	uint8_t version;
-	[data getBytes:&version length:1];
-	if (version == INSOCKS5HandshakeVersion5) { // SOCKS Protocol Version 5
-		[_socket readDataToLength:1 withTimeout:INSOCKS5SocketTimeout tag:INSOCKS5HandshakePhaseNumberOfAuthenticationMethods];
+	void(^failureBlock)() = ^{
+		[self refuseConnectionWithErrorDescription:@"Invalid SOCKS protocol version."];
+	};
+	if ([data length] == length) {
+		uint8_t version;
+		[data getBytes:&version length:length];
+		if (version == INSOCKS5HandshakeVersion5) { // SOCKS Protocol Version 5
+			[self readDataForSOCKS5Tag:INSOCKS5HandshakePhaseNumberOfAuthenticationMethods];
+		} else {
+			failureBlock();
+		}
 	} else {
-		[self sendSOCKSHandshakeCommandNotSupportedResponse];
-		[self notifySOCKS5HandshakeErrorWithDescription:@"Invalid SOCKS protocol version."];
+		failureBlock();
 	}
 }
 
-- (void)readSOCKS5NumberOfAuthenticationMethodsFromData:(NSData *)data
+- (void)readSOCKS5NumberOfAuthenticationMethodsFromData:(NSData *)data expectedLength:(NSUInteger)length
 {
-	[data getBytes:&_numberOfAuthenticationMethods length:1];
-	[_socket readDataToLength:_numberOfAuthenticationMethods withTimeout:INSOCKS5SocketTimeout tag:INSOCKS5HandshakePhaseAuthenticationMethod];
+	if ([data length] == length) {
+		[data getBytes:&_numberOfAuthenticationMethods length:length];
+		[_socket readDataToLength:_numberOfAuthenticationMethods withTimeout:INSOCKS5SocketTimeout tag:INSOCKS5HandshakePhaseAuthenticationMethod];
+	} else {
+		[self refuseConnectionWithErrorDescription:@"Unable to retrieve number of authentication methods."];
+	}
 }
 
 - (void)readSOCKS5AuthenticationMethodsFromData:(NSData *)data
 {
 	uint8_t authMethods[_numberOfAuthenticationMethods];
-	if (sizeof(authMethods) == [data length]) {
+	if ([data length] == sizeof(authMethods)) {
 		BOOL hasSupportedAuthMethod = NO;
 		// TODO: Add support for username/password authentication as well
 		for (int i = 0; i < sizeof(authMethods); i++) {
@@ -300,48 +335,124 @@ static uint8_t const INSOCKS5HandshakeVersion5 = 0x05;
 			}
 		}
 		if (hasSupportedAuthMethod) {
-			[self sendSOCKS5HandshakeSucceededResponse];
-			[_socket readDataToLength:3 withTimeout:INSOCKS5SocketTimeout tag:INSOCKS5RequestPhaseHeaderFragment];
+			[self readDataForSOCKS5Tag:INSOCKS5RequestPhaseHeaderFragment];
 		} else {
-			[self sendSOCKSHandshakeCommandNotSupportedResponse];
-			[self notifySOCKS5HandshakeErrorWithDescription:@"No supported authentication method."];
+			[self refuseConnectionWithErrorDescription:@"No supported authentication method."];
 		}
 		
 	} else {
-		[self sendSOCKSHandshakeCommandNotSupportedResponse];
-		[self notifySOCKS5HandshakeErrorWithDescription:@"Could not read authentication methods"];
+		[self refuseConnectionWithErrorDescription:@"Could not read authentication methods"];
 	}
 }
 
-- (void)readSOCKS5HeaderFragmentFromData:(NSData *)data
+- (void)readSOCKS5HeaderFragmentFromData:(NSData *)data expectedLength:(NSUInteger)length
 {
-	
+	if ([data length] == length) {
+		uint8_t header[length];
+		[data getBytes:&header length:length];
+		
+		uint8_t version = header[0];
+		if (version != INSOCKS5HandshakeVersion5) {
+			[self refuseConnectionWithErrorDescription:@"Invalid SOCKS protocol version."];
+			return;
+		}
+		
+		_requestCommandCode = header[1];
+		// Third byte is just a reserved paramter (0x00);
+		[self readDataForSOCKS5Tag:INSOCKS5RequestPhaseAddressType];
+	} else {
+		[self refuseConnectionWithErrorDescription:@"Could not read request header."];
+	}
 }
 
-- (void)readSOCKS5AddressTypeFromData:(NSData *)data
+- (void)readSOCKS5AddressTypeFromData:(NSData *)data expectedLength:(NSUInteger)length
 {
-	
+	if ([data length] == length) {
+		_addressData = [NSMutableData dataWithData:data];
+		uint8_t addressType;
+		[data getBytes:&addressType length:length];
+		
+		switch (addressType) {
+			case INSOCKS5AddressTypeIPv4:
+				[self readDataForSOCKS5Tag:INSOCKS5RequestPhaseIPv4Address];
+				break;
+			case INSOCKS5AddressTypeIPv6:
+				[self readDataForSOCKS5Tag:INSOCKS5RequestPhaseIPv6Address];
+				break;
+			case INSOCKS5AddressTypeDomainName:
+				[self readDataForSOCKS5Tag:INSOCKS5RequestPhaseDomainNameLength];
+				break;
+			default:
+				break;
+		}
+	} else {
+		[self refuseConnectionWithErrorDescription:@"Could not read address type."];
+	}
 }
 
-- (void)readSOCKS5IPv4AddressFromData:(NSData *)data
+- (void)readSOCKS5IPv4AddressFromData:(NSData *)data expectedLength:(NSUInteger)length
 {
-	
+	if ([data length] == length) {
+		uint8_t address[length];
+		[data getBytes:&address length:length];
+		[_addressData appendBytes:address length:length];
+		char ip[INET_ADDRSTRLEN];
+		_targetHost = [NSString stringWithCString:inet_ntop(AF_INET, address, ip, sizeof(ip)) encoding:NSUTF8StringEncoding];
+		[self readDataForSOCKS5Tag:INSOCKS5RequestPhasePort];
+	} else {
+		[self refuseConnectionWithErrorDescription:@"Could not read IPv4 address."];
+	}
 }
 
-- (void)readSOCKS5IPv6AddressFromData:(NSData *)data
+- (void)readSOCKS5IPv6AddressFromData:(NSData *)data expectedLength:(NSUInteger)length
 {
-	
+	if ([data length] == length) {
+		uint8_t address[length];
+		[data getBytes:&address length:length];
+		[_addressData appendBytes:address length:length];
+		char ip[INET6_ADDRSTRLEN];
+		_targetHost = [NSString stringWithCString:inet_ntop(AF_INET6, address, ip, sizeof(ip)) encoding:NSUTF8StringEncoding];
+		[self readDataForSOCKS5Tag:INSOCKS5RequestPhasePort];
+	} else {
+		[self refuseConnectionWithErrorDescription:@"Could not read IPv6 address."];
+	}
+}
+
+- (void)readSOCKS5DomainNameLengthFromData:(NSData *)data expectedLength:(NSUInteger)length
+{
+	if ([data length] == length) {
+		[data getBytes:&_domainNameLength length:length];
+		[_addressData appendBytes:(void *)_domainNameLength length:length];
+		[self readDataForSOCKS5Tag:INSOCKS5RequestPhaseDomainName];
+	} else {
+		[self refuseConnectionWithErrorDescription:@"Could not read domain name length."];
+	}
 }
 
 - (void)readSOCKS5DomainNameFromData:(NSData *)data
 {
-	
+	if ([data length] == _domainNameLength) {
+		uint8_t domainName[_domainNameLength];
+		[data getBytes:&domainName length:_domainNameLength];
+		[_addressData appendBytes:domainName length:_domainNameLength];
+		_targetHost = [[NSString alloc] initWithBytes:domainName length:_domainNameLength encoding:NSUTF8StringEncoding];
+	} else {
+		[self refuseConnectionWithErrorDescription:@"Could not read domain name"];
+	}
 }
 
-- (void)readSOCKS5PortFromData:(NSData *)data
+- (void)readSOCKS5PortFromData:(NSData *)data expectedLength:(NSUInteger)length
 {
-	
+	if ([data length] == length) {
+		uint8_t port[length];
+		[data getBytes:&port length:length];
+		[_addressData appendBytes:port length:length];
+		_targetPort = (port[0] << 8 | port[1]);
+	} else {
+		[self refuseConnectionWithErrorDescription:@"Could not read port."];
+	}
 }
+
 #pragma mark - Private
 
 // Notifies delegate of an error during the SOCKS5 handshake and disconnects the socket
@@ -353,9 +464,15 @@ static uint8_t const INSOCKS5HandshakeVersion5 = 0x05;
 	[_socket disconnectAfterWriting];
 }
 
+- (void)refuseConnectionWithErrorDescription:(NSString *)description
+{
+	[self sendSOCKSHandshakeConnectionRefusedResponse];
+	[self notifySOCKS5HandshakeErrorWithDescription:description];
+}
+
 + (NSData *)replyDataForResponseType:(INSOCKS5HandshakeReplyType)type
 {
-	const unsigned char bytes[3] = {0x05, type, 0x00};
+	const unsigned char bytes[3] = {INSOCKS5HandshakeVersion5, type, 0x00}; // 0x00 is a reserved parameter
 	return [NSData dataWithBytes:bytes length:3];
 }
 
@@ -364,8 +481,38 @@ static uint8_t const INSOCKS5HandshakeVersion5 = 0x05;
 	[_socket writeData:[self.class replyDataForResponseType:INSOCKS5HandshakeReplySucceeded] withTimeout:INSOCKS5SocketTimeout tag:0];
 }
 
-- (void)sendSOCKSHandshakeCommandNotSupportedResponse
+- (void)sendSOCKSHandshakeConnectionRefusedResponse
 {
 	[_socket writeData:[self.class replyDataForResponseType:INSOCKS5HandshakeReplyConnectionRefused] withTimeout:INSOCKS5SocketTimeout tag:0];
+}
+
++ (NSUInteger)dataLengthForSOCKS5Tag:(NSUInteger)tag
+{
+	switch (tag) {
+		case INSOCKS5HandshakePhaseVersion:
+		case INSOCKS5HandshakePhaseNumberOfAuthenticationMethods:
+		case INSOCKS5RequestPhaseAddressType:
+		case INSOCKS5RequestPhaseDomainNameLength:
+			return 1;
+		case INSOCKS5RequestPhaseHeaderFragment:
+			return 3;
+		case INSOCKS5RequestPhaseIPv4Address:
+			return 4;
+		case INSOCKS5RequestPhaseIPv6Address:
+			return 16;
+		case INSOCKS5RequestPhasePort:
+			return 2;
+		default:
+			return 0;
+			break;
+	}
+}
+
+- (void)readDataForSOCKS5Tag:(NSInteger)tag
+{
+	NSUInteger dataLength = [self.class dataLengthForSOCKS5Tag:tag];
+	if (dataLength) {
+		[_socket readDataToLength:dataLength withTimeout:INSOCKS5SocketTimeout tag:tag];
+	}
 }
 @end
